c7bb9624a3ed077907da5a8edf92b4c1e042baf7
Anth0ny29 <43807800+Anth0ny29@users.noreply.github.com>
2020-10-15 21:26:48 +0200

    Install packages (#2)
    Debug fonction d'installation de package

--- GetSubDomainInfo.sh
+++ GetSubDomainInfo.sh
@@ -27,16 +27,16 @@ install_packages(){
 	cat /etc/os-release |grep "arch" 2> /dev/null > /dev/null #Check si l'OS est de type Arch Linux
 	if [ $? -eq 0 ]; then
 		
-		pacman -Qi dnsutils nmap 2>/dev/null > /dev/null
+		pacman -Qi dnsutils nmap whois 2>/dev/null > /dev/null
 		if [ $? -eq 0 ]; then
 			echo -e "	${GREEN}Dependances => OK${NOCOLOR}"
 			return 0
 		
 		else
 
-		sudo pacman --noconfirm -S 'bind' nmap 2>/dev/null > /dev/null
+		sudo pacman --noconfirm -S 'bind' nmap whois > /dev/null
 			if [ $? -eq 0 ]; then
-				
+				clear
 				echo -e "	${GREEN}Dépendances => installées${NOCOLOR}"
 				return 0
 			else
@@ -50,16 +50,17 @@ install_packages(){
 	cat /etc/os-release |grep "Ubuntu\|debian" 2> /dev/null > /dev/null #Check si l'OS est de type Ubuntu
 	if [ $? -eq 0 ]; then
 		
-		dpkg -l dnsutils nmap 2>/dev/null > /dev/null
+		dpkg -s dnsutils nmap whois 2>/dev/null > /dev/null
 		if [ $? -eq 0 ]; then
 			echo -e "	${GREEN}Dépendances => OK${NOCOLOR}"
 			return 0
 		
 		else
 
-		sudo apt update && sudo apt install -y dnsutils nmap 2>/dev/null > /dev/null
+		sudo apt update > /dev/null && sudo apt install -y dnsutils nmap whois > /dev/null
 			if [ $? -eq 0 ]; then
 				
+				clear
 				echo -e "	${GREEN}Dépendances => installées${NOCOLOR}"
 				return 0
 			else
