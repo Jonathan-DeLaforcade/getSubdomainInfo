73d724ec3e59c2a0a3cb1c5a9607320baf8e0d04
Anth0ny29 <contact@ferrero-anthony.fr>
2020-10-17 17:38:19 +0200

    Modification de la localisation en cas d'impossibilité de localiser l'IP

--- GetSubDomainInfo.sh
+++ GetSubDomainInfo.sh
@@ -97,12 +97,12 @@ getIPandLocate(){
 	echo ""
 	Liste_IPV4=($(nslookup $1 |grep "Address" |cut -d' ' -f2 | grep -E "(([0-9]+\.[0-9]+\.[0-9]+\.[0-9]+$))"))
 	Liste_IPV6=($(nslookup $1 |grep "Address" |cut -d' ' -f2 | grep -E "(([0-9a-fA-F]{1,4}:){7,7}[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,7}:|([0-9a-fA-F]{1,4}:){1,6}:[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,5}(:[0-9a-fA-F]{1,4}){1,2}|([0-9a-fA-F]{1,4}:){1,4}(:[0-9a-fA-F]{1,4}){1,3}|([0-9a-fA-F]{1,4}:){1,3}(:[0-9a-fA-F]{1,4}){1,4}|([0-9a-fA-F]{1,4}:){1,2}(:[0-9a-fA-F]{1,4}){1,5}|[0-9a-fA-F]{1,4}:((:[0-9a-fA-F]{1,4}){1,6})|:((:[0-9a-fA-F]{1,4}){1,7}|:)|fe80:(:[0-9a-fA-F]{0,4}){0,4}%[0-9a-zA-Z]{1,}|::(ffff(:0{1,4}){0,1}:){0,1}((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])\.){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])|([0-9a-fA-F]{1,4}:){1,4}:((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])\.){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9]))"))
-	
+	echo ""
 	for IPV4 in "${Liste_IPV4[@]}"
 		do
 			localisation=$(timeout --foreground 5 curl -s http://ip-api.com/json/$IPV4)
 			localisation=$(echo $localisation | grep -Eo "city.*" | cut -d'"' -f 3)
-			echo -e "	${GREEN}[+]${NOCOLOR} IPV4 : $IPV4"
+			echo -e "	${GREEN}[+]${NOCOLOR} IPV4 : ${GREEN}$IPV4${NOCOLOR}"
 			
 			if [ "$localisation" == *"fail"* ]; then
 				echo -e "		${YELLOW}[+]${NOCOLOR} Localisation : ${RED}Indisponible${NOCOLOR}"
@@ -123,7 +123,7 @@ getIPandLocate(){
 		do
 			localisation=$(timeout --foreground 5 curl -s http://ip-api.com/json/$IPV6)
 			localisation=$(echo $localisation | grep -Eo "city.*" | cut -d'"' -f 3)
-			echo -e "	${GREEN}[+]${NOCOLOR} IPV6 : $IPV6"
+			echo -e "	${GREEN}[+]${NOCOLOR} IPV6 : ${GREEN}$IPV6${NOCOLOR}"
 			
 			if [ "$localisation" == *"fail"* ]; then
 				echo -e "		${YELLOW}[+]${NOCOLOR} Localisation : ${RED}Indisponible${NOCOLOR}"
